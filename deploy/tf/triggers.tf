d607641b5ffb33b0e4b64df0ece67b9d301c2a2d
Ayub Adan <ayub.adanwarsame@sky.uk>
2025-11-28 16:00:04 +0000

    commenedted out cloudbuild.yaml

--- deploy/tf/triggers.tf
+++ deploy/tf/triggers.tf
@@ -1,50 +1,50 @@
-# Trigger for application deployment to Cloud Run
-resource "google_cloudbuild_trigger" "app_cicd_trigger" {
-  project = var.project_id
-  location = var.cb_region # CB has quote restrictions in certain regions
-  name    = "deploy-${var.service_name}-svc"
-  service_account = resource.google_service_account.cicd_runner_sa.id
-  description = "Trigger for ${var.trigger_branch_name} application deployment"
-
-  repository_event_config {
-    repository = google_cloudbuildv2_repository.repo_app.id
-    push {
-      branch = "^${var.trigger_branch_name}$"
-    }
-  }
-
-  filename = "cloudbuild.yaml"
-  included_files = [
-    "src/**",
-    "tests/**",
-  ]
-
-  ignored_files   = ["README.md"]
-
-  # Define substitutions - these override defaults in cloudbuild.yaml
-  substitutions = {
-    _DEPLOY_REGION                 = var.region
-    _CB_REGION                     = var.cb_region
-    _AR_HOSTNAME                   = "${var.cb_region}-docker.pkg.dev"
-    _PLATFORM                      = "managed"
-    _SERVICE_NAME                  = var.service_name
-    _LOG_LEVEL                     = var.log_level
-    _MAX_INSTANCES                 = "1"
-    _CICD_RUNNER_SA_EMAIL           = "${var.cicd_runner_sa_name}@${var.project_id}.iam.gserviceaccount.com"
-  }
-
-  depends_on = [resource.google_project_service.apis, google_cloudbuildv2_repository.repo_app]
-
-  tags = [
-    var.service_name, 
-    var.trigger_branch_name # Tag with the branch/environment
-  ]
-
-  lifecycle {
-    # Prevent accidental deletion if the trigger is manually modified
-    prevent_destroy = false # Set to true in production if desired
-  }
-}
+# # Trigger for application deployment to Cloud Run
+# resource "google_cloudbuild_trigger" "app_cicd_trigger" {
+#   project = var.project_id
+#   location = var.cb_region # CB has quote restrictions in certain regions
+#   name    = "deploy-${var.service_name}-svc"
+#   service_account = resource.google_service_account.cicd_runner_sa.id
+#   description = "Trigger for ${var.trigger_branch_name} application deployment"
+#
+#   repository_event_config {
+#     repository = google_cloudbuildv2_repository.repo_app.id
+#     push {
+#       branch = "^${var.trigger_branch_name}$"
+#     }
+#   }
+#
+#   filename = "cloudbuild.yaml"
+#   included_files = [
+#     "src/**",
+#     "tests/**",
+#   ]
+#
+#   ignored_files   = ["README.md"]
+#
+#   # Define substitutions - these override defaults in cloudbuild.yaml
+#   substitutions = {
+#     _DEPLOY_REGION                 = var.region
+#     _CB_REGION                     = var.cb_region
+#     _AR_HOSTNAME                   = "${var.cb_region}-docker.pkg.dev"
+#     _PLATFORM                      = "managed"
+#     _SERVICE_NAME                  = var.service_name
+#     _LOG_LEVEL                     = var.log_level
+#     _MAX_INSTANCES                 = "1"
+#     _CICD_RUNNER_SA_EMAIL           = "${var.cicd_runner_sa_name}@${var.project_id}.iam.gserviceaccount.com"
+#   }
+#
+#   depends_on = [resource.google_project_service.apis, google_cloudbuildv2_repository.repo_app]
+#
+#   tags = [
+#     var.service_name,
+#     var.trigger_branch_name # Tag with the branch/environment
+#   ]
+#
+#   lifecycle {
+#     # Prevent accidental deletion if the trigger is manually modified
+#     prevent_destroy = false # Set to true in production if desired
+#   }
+# }
 
 # Trigger for infrastructure deployment using Terraform
 resource "google_cloudbuild_trigger" "tf_trigger" {
@@ -60,7 +60,7 @@ resource "google_cloudbuild_trigger" "tf_trigger" {
       branch = "^${var.trigger_branch_name}$"
     }
   }
-  # ⭐ INLINE BUILD STEPS (instead of cloudbuild-tf.yaml)
+  # INLINE BUILD STEPS (instead of cloudbuild-tf.yaml)
   build {
     step {
       id = "TF Init"
